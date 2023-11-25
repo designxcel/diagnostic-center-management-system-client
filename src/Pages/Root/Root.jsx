a8fe83f9d1f77a4c523fd952e52c412bfe2c88b7
designxcel <designxcel08@gmail.com>
2023-11-25 11:19:35 +0600

    added a blog page and testimonial section

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import Footer from "../Shared/Footer/Footer";
 import FooterBottom from "../Shared/Footer/FooterBottom";
+import Testimonial from "../Testimonial/Testimonial";
 
 
 const Root = () => {
@@ -9,6 +10,7 @@ const Root = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Testimonial></Testimonial>
             <Footer></Footer>
             <FooterBottom></FooterBottom>
         </div>
---
+++ src/Pages/Testimonial/Testimonial.jsx
@@ -0,0 +1,54 @@
+import UseAxiosPublic from '../../Hooks/UseAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import '@smastrom/react-rating/style.css'
+import { Navigation } from 'swiper/modules';
+import { FaQuoteLeft } from 'react-icons/fa';
+import { Rating } from '@smastrom/react-rating';
+import { useState } from 'react';
+
+
+const Testimonial = () => {
+    const [rating, setRating] = useState(0)
+    const axiosPublic = UseAxiosPublic()
+    const { data: reviews = [] } = useQuery({
+    queryKey: ["reviews"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/reviews");
+      return res.data;
+    },
+  });
+    return (
+        <div className='p-20'>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            {
+                reviews.map(review =><SwiperSlide className="px-20 space-y-6" key={review._id}>
+                <div className="flex justify-center">
+                    <Rating
+                    style={{ maxWidth: 180 }}
+                    value={review.rating}
+                    readOnly
+                    />
+                </div>
+                <div className="avatar flex justify-center items-center">
+                    <div className="w-16 rounded-full">
+                        <img src={review.image} />
+                    </div>
+                </div>
+                <div className="text-5xl flex justify-center">
+                    <FaQuoteLeft></FaQuoteLeft>
+                </div>
+                <p className='max-w-7xl mx-auto'>{review.desc}</p>
+                <h2 className="text-3xl text-center font-bold uppercase text-gray-700">{review.name}</h2>
+            </SwiperSlide>)
+            }
+            </Swiper>
+        </div>
+    );
+};
+
+export default Testimonial;
